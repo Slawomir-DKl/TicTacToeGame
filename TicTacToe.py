a18eff678ddc978d048f4eb016d3bc0f5df0c0cb
Sławomir Duda-Klimaszewski <slawomir@d-kl.pl>
2022-10-21 12:39:54 +0200

    Introduce triples variable Simplify victory checking function Make the board smaller Comment defense move Correct bug when there is no free fields

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,20 +1,19 @@
 def display_board(board):  # Printing the current board
 
     # Board layout elements
-    horiz = "+-------+-------+-------+"
-    vert = "|       |       |       |"
-    lft = "|  "
-    btwn = "  |  "
-    rght = "  |"
+    horiz = "+-----+-----+-----+"
+    lft = "| "
+    btwn = " | "
+    rght = " |"
 
     # Printing the board
-    print(horiz, vert, sep='\n')
+    print(horiz, sep='\n')
     print(lft, board[0][0], btwn, board[0][1], btwn, board[0][2], rght)
-    print(vert, horiz, vert, sep='\n')
+    print(horiz, sep='\n')
     print(lft, board[1][0], btwn, board[1][1], btwn, board[1][2], rght)
-    print(vert, horiz, vert, sep='\n')
+    print(horiz, sep='\n')
     print(lft, board[2][0], btwn, board[2][1], btwn, board[2][2], rght)
-    print(vert, horiz, sep='\n')
+    print(horiz, sep='\n')
 
 
 def enter_move(board):  # User's move & board update
@@ -62,42 +61,16 @@ def make_list_of_free_fields(board):  # Building list of all the free squares
 
 def victory_check(board, sign, towin):  # Checking if anyone has [towin] number of hits in a row (if not needed for winning algorithm, delete towin)
 
-    win = 0
-    # Check for horizontal win
-    for x in range(3):
-        for y in range(3):
-            if board[x][y] == sign:
-                win += 1
-        if win == towin:
-            return "win"
-        else:
-            win = 0
-    # Check for vertical win
-    for x in range(3):
-        for y in range(3):
-            if board[y][x] == sign:
-                win += 1
-        if win == towin:
-            return "win"
-        else:
-            win = 0
-    # Check for 1st diagonal win
-    for x in range(3):
-        if board[x][x] == sign:
-            win += 1
-    if win == towin:
-        return "win"
-    else:
+    # Checking for win in any triple
+    for i in range(len(triples)):
         win = 0
-    # Check for 2nd diagonal win
-    if board[0][2] == sign:
-        win += 1
-    if board[1][1] == sign:
-        win += 1
-    if board[2][0] == sign:
-        win += 1
-    if win == towin:
-        return "win"
+        for j in range(3):
+            if board[triples[i][j][0]][triples[i][j][1]] == sign:
+                win += 1
+        if win == towin:
+            return "win"
+        else:
+            win = 0
     return "next"
 
 def win_alg(board):
@@ -112,29 +85,34 @@ def win_alg(board):
             if board[x][y] == "X":
                 row.append(1)
                 win += 1
+#                print(row, win)
             elif board[x][y] == "O":
                 row.append(0)
+#                print(row, win)
         if len(row) == 2: # We're interested only in rows with 2 values
             if win == 2: # Computer has 2 in the row, user has 0 - winning movement
                 for y in range(3):
                     if board[x][y] not in ["X", "O"]:
                         board[x][y] = "X"
+#                        print(row, win, "x = ", x, "y = ", y)
                         return "ok"
-            elif win == 0: # User has 2 in the row, computer has 0 - defence
-                for y in range(3):
-                    if board[x][y] not in ["X", "O"]:
-                        board[x][y] = "X"
-                        return "ok"
+ #           elif win == 0: # User has 2 in the row, computer has 0 - defense
+ #               for y in range(3):
+ #                   if board[x][y] not in ["X", "O"]:
+ #                       board[x][y] = "X"
+ #                       print(row, win, "ok")
+ #                       return "ok"
 
 def draw_move(board):  # Computer's move & board update
     if win_alg(board) != "ok":
         from random import randrange
         free_fields = make_list_of_free_fields(board)
-        comp_move = randrange(len(free_fields))
-        field = free_fields[comp_move]  # tuple containing coordinates of computer move
-        x = field[0]
-        y = field[1]
-        board[x][y] = "X"
+        if len(free_fields) != 0:
+            comp_move = randrange(len(free_fields))
+            field = free_fields[comp_move]  # tuple containing coordinates of computer move
+            x = field[0]
+            y = field[1]
+            board[x][y] = "X"
 
 
 # Initialization - empty board - list [row][column]
@@ -146,6 +124,10 @@ board.append(row)
 row = [x for x in range(7, 10)]
 board.append(row)
 display_board(board)
+
+# List of all possible 3-field lines
+triples = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+           ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)), ((0, 0), (1, 1), (2, 2)), ((2, 0), (1, 1), (0, 2)))
 
 # Randomly selecting who starts
 from random import randrange
@@ -182,3 +164,4 @@ while True:
 # Implementation of winning algorithm acc. to:
 # https://swistak.codes/post/algorytmika-gier-kolko-i-krzyzyk/#strategia-wygrywania-w-kółko-i-krzyżyk
 # on the basis of https://onlinelibrary.wiley.com/doi/abs/10.1207/s15516709cog1704_3
+
