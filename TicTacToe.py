8f10ea38d690b3255ca641e17417de0db8373a44
Sławomir Duda-Klimaszewski <slawomir@d-kl.pl>
2022-10-24 15:07:31 +0200

    Add function count_xo for counting marked fields in a line Simplify functions by replacing similar code with count_xo function Add comments with the rest of the winning algorithm Add 3rd part of the winning algorithm - occupy intersection of the fork Add random move indication to be printed if movement is not from winning algorithm Add intersections variable with all possible intersecting lines

--- TicTacToe.py
+++ TicTacToe.py
@@ -75,18 +75,23 @@ def victory_check(board, sign, towin):  # Checking if anyone has [towin] number 
             win = 0
     return "next"
 
+def count_xo(board, i): #counting marked fields in a line
+
+    xfld = 0
+    ofld = 0
+    for j in range(3):
+        x = triples[i][j][0]  # Coordinates of the checked field
+        y = triples[i][j][1]
+        if board[x][y] == "X":
+            xfld += 1
+        elif board[x][y] == "O":
+            ofld += 1
+    return xfld, ofld
+
 def win_block(board, xwin, owin): # if there are two identically checked fields in a row and the third field is empty, check the third one and win or block
     for i in range(len(triples)):
-        xfld = 0
-        ofld = 0
-        for j in range(3):
-            x = triples[i][j][0] #Coordinates of the checked field
-            y = triples[i][j][1]
-            if board[x][y] == "X":
-                xfld += 1
-            elif board[x][y] == "O":
-                ofld += 1
-        if xfld == xwin and ofld == owin: # Computer has 2 in the row, user has 0 - winning movement
+        xfld, ofld = count_xo(board, i)
+        if xfld == xwin and ofld == owin: # Computer has 2 in the row, user has 0 - winning movement; computer has 0 in the row, user has 2 - blocking movement
             for j in range(3):
                 x = triples[i][j][0]
                 y = triples[i][j][1]
@@ -98,16 +103,51 @@ def win_alg(board):
     # Implementation of winning algorithm acc. to:
     # https://swistak.codes/post/algorytmika-gier-kolko-i-krzyzyk/#strategia-wygrywania-w-kółko-i-krzyżyk
     # on the basis of https://onlinelibrary.wiley.com/doi/abs/10.1207/s15516709cog1704_3
-    # 1. Winning move - if the computer has two checked fields in a row and the third field is empty, check the third one and win
+    # 1. Winning move - if the computer has two checked fields in a row and the third field is empty,
+    # check the third one and win
     if win_block(board, 2, 0) == "ok":
         return "ok"
-    # 2. Block - if the opponent has two checked fields in a row and the third field is empty, check the third one and block the opponent
+    # 2. Block - if the opponent has two checked fields in a row and the third field is empty,
+    # check the third one and block the opponent
     if win_block(board, 0, 2) == "ok":
         return "ok"
+    # 3. Do a fork - if you have two intersecting lines (horizontal, vertical, diagonal) with one your mark and with two
+    # empty places AND if the intersection of the lines is empty, insert your mark in the intersection
+    # (to create two possibilities to win in the next move)
+    for i in range(len(triples)):
+        xfld, ofld = count_xo(board, i)
+        if xfld == 1 and ofld == 0: # first line with 1 computer mark and 0 user marks
+            for k in range(len(intersections[i])): # checking all intersecting lines
+                l = intersections[i][k] # number of intersecting line to check
+                xfldint, ofldint = count_xo(board, l)
+                if xfldint == 1 and ofldint == 0: # intersecting line with 1 computer mark and 0 user marks
+                    for n in range(3):
+                        for o in range(3):
+                            if triples[i][n] == triples[l][o]:
+                                x = triples[i][n][0]  # Coordinates of the checked field
+                                y = triples[i][n][1]
+                                if board[x][y] not in ("O", "X"):
+                                    board[x][y] = "X"
+                                    return "ok"
+    return
 
+    # 4. Do a blocking fork - if you have two intersecting lines (horizontal, vertical, diagonal) with one opponent mark
+    # and with two empty places AND if the intersection of the lines is empty, then: if there is empty place which would
+    # create two marks in a row for me (that the opponent has to block in his next move) - insert your mark in this place
+    # else: insert your mark in the intersection (to block two possibilities to win for the opponent in the next move)
+
+    # 5. Use the center - if the center field is empty, insert your mark here
+
+    # 6. Play the opposite corner - if the opponent has its mark in the corner and the opposite corner is empty,
+    # insert your mark in the opposite corner
+
+    # 7. Occupy the empty corner - if there is an empty corner, insert your mark here
+
+    # 8. Play on the empty side - if there is an empty side field, insert your mark here
 
 def draw_move(board):  # Computer's move & board update
     if win_alg(board) != "ok":
+        print("random move")
         from random import randrange
         free_fields = make_list_of_free_fields(board)
         if len(free_fields) != 0:
@@ -131,7 +171,9 @@ display_board(board)
 # List of all possible 3-field lines
 triples = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
            ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)), ((0, 0), (1, 1), (2, 2)), ((2, 0), (1, 1), (0, 2)))
-
+# All possible intersecting lines in relation to triples
+intersections = ((3, 4, 5, 6, 7), (3, 4, 5, 6, 7), (3, 4, 5, 6, 7), (6, 7), (6, 7), (6, 7), (7,),
+                 ())
 # Randomly selecting who starts
 from random import randrange
 
@@ -139,7 +181,7 @@ who = randrange(2)
 if who == 0:
     compsign = "X"
     print("Computer begins and plays with X")
-    draw_move(board)  # First computer move is also random
+    draw_move(board)  # First computer move is also algorythmised
 else:
     compsign = "O"
     print("You begin and play with O")
