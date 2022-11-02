c30dec907284533bad8ed7616e184019d4ba087c
Sławomir Duda-Klimaszewski <slawomir@d-kl.pl>
2022-11-02 21:56:06 +0100

    Correct more mistakes according to PyCharm recommendations Rename variables to more meaningful names Extend except clause

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,7 +1,7 @@
 from random import randrange
 
 
-def display_board(board):  # Printing the current board
+def display_board(fnboard):  # Printing the current board
 
     # Board layout elements
     horiz = "+-----+-----+-----+"
@@ -11,15 +11,15 @@ def display_board(board):  # Printing the current board
 
     # Printing the board
     print(horiz, sep='\n')
-    print(lft, board[0][0], btwn, board[0][1], btwn, board[0][2], rght)
+    print(lft, fnboard[0][0], btwn, fnboard[0][1], btwn, fnboard[0][2], rght)
     print(horiz, sep='\n')
-    print(lft, board[1][0], btwn, board[1][1], btwn, board[1][2], rght)
+    print(lft, fnboard[1][0], btwn, fnboard[1][1], btwn, fnboard[1][2], rght)
     print(horiz, sep='\n')
-    print(lft, board[2][0], btwn, board[2][1], btwn, board[2][2], rght)
+    print(lft, fnboard[2][0], btwn, fnboard[2][1], btwn, fnboard[2][2], rght)
     print(horiz, sep='\n')
 
 
-def enter_move(board):  # User's move & board update
+def enter_move(fnboard):  # User's move & board update
 
     # Asking user for input and checking it
     while True:
@@ -30,31 +30,33 @@ def enter_move(board):  # User's move & board update
                 chk = move / (move - move)  # Generating ZeroDivisionError
             # Calculating row and column @
             if move % 3 == 0:
-                r = (move - move % 3) // 3 - 1
-                c = 2
+                x = (move - move % 3) // 3 - 1
+                y = 2
             else:
-                r = (move - move % 3) // 3
-                c = move % 3 - 1
-            if board[r][c] == move:  # Checking if the square is empty
+                x = (move - move % 3) // 3
+                y = move % 3 - 1
+            if fnboard[x][y] == move:  # Checking if the square is empty
                 # Updating square
-                board[r][c] = "O"
+                fnboard[x][y] = "O"
                 break
             else:
                 chk = nofunct  # non-existing function to generate the ValueError
         except ValueError:
             print("It is not an integer!")
         except NameError:
-            print("This square is occupied")
-        except:
-            print("The number is not in the range from 1 to 9")
+            print("This square is occupied. Enter the number of empty square.")
+        except ZeroDivisionError:
+            print("The number is not in the range from 1 to 9. Enter higher number.")
+        except IndexError:
+            print("The number is not in the range from 1 to 9. Enter lower number.")
 
 
-def make_list_of_free_fields(board):  # Building list of all the free squares
+def make_list_of_free_fields(fnboard):  # Building list of all the free squares
 
     free = []
     for x in range(3):
         for y in range(3):
-            if board[x][y] == "X" or board[x][y] == "O":
+            if fnboard[x][y] == "X" or fnboard[x][y] == "O":
                 continue
             else:
                 pair = (x, y)  # tuple acc. to the task
@@ -62,95 +64,93 @@ def make_list_of_free_fields(board):  # Building list of all the free squares
     return free
 
 
-def victory_check(board, sign, towin):  # Checking if anyone has [towin] number of hits in a row (if not needed for
+def victory_check(fnboard, sign, towin):  # Checking if anyone has [towin] number of hits in a row (if not needed for
     # winning algorithm, delete towin)
 
     # Checking for win in any triple
-    for i in range(len(triples)):
+    for triple in range(len(triples)):
         win = 0
-        for j in range(3):
-            x = triples[i][j][0]  # Coordinates of the checked field
-            y = triples[i][j][1]
-            if board[x][y] == sign:
+        for field in range(3):
+            x = triples[triple][field][0]  # Coordinates of the checked field
+            y = triples[triple][field][1]
+            if fnboard[x][y] == sign:
                 win += 1
         if win == towin:
             return "win"
-        else:
-            win = 0
     return "next"
 
 
-def count_xo(board, i):  # counting marked fields in a line
+def count_xo(fnboard, triple):  # counting marked fields in a line
 
     xfld = 0
     ofld = 0
-    for j in range(3):
-        x = triples[i][j][0]  # Coordinates of the checked field
-        y = triples[i][j][1]
-        if board[x][y] == "X":
+    for field in range(3):
+        x = triples[triple][field][0]  # Coordinates of the checked field
+        y = triples[triple][field][1]
+        if fnboard[x][y] == "X":
             xfld += 1
-        elif board[x][y] == "O":
+        elif fnboard[x][y] == "O":
             ofld += 1
     return xfld, ofld
 
 
-def win_block(board, xwin, owin):  # if there are two identically checked fields in a row and the third field is empty,
+def win_block(fnboard, xwin, owin):  # if there are two identically checked fields in a row and the 3rd field is empty,
     # check the third one and win or block
-    for i in range(len(triples)):
-        xfld, ofld = count_xo(board, i)
+    for triple in range(len(triples)):
+        xfld, ofld = count_xo(fnboard, triple)
         if xfld == xwin and ofld == owin:  # Computer has 2 in the row, user has 0 - winning movement; computer has 0
             # in the row, user has 2 - blocking movement
-            for j in range(3):
-                x = triples[i][j][0]
-                y = triples[i][j][1]
-                if board[x][y] not in ["X", "O"]:
-                    board[x][y] = "X"
+            for field in range(3):
+                x = triples[triple][field][0]
+                y = triples[triple][field][1]
+                if fnboard[x][y] not in ["X", "O"]:
+                    fnboard[x][y] = "X"
                     return "ok"
 
 
-def win_alg(board):
+def win_alg(fnboard):
     # Implementation of winning algorithm acc. to:
     # https://swistak.codes/post/algorytmika-gier-kolko-i-krzyzyk/#strategia-wygrywania-w-kółko-i-krzyżyk
     # on the basis of https://onlinelibrary.wiley.com/doi/abs/10.1207/s15516709cog1704_3
     # 1. Winning move - if the computer has two checked fields in a row and the third field is empty,
     # check the third one and win
-    if win_block(board, 2, 0) == "ok":
+    if win_block(fnboard, 2, 0) == "ok":
         return "ok"
     # 2. Block - if the opponent has two checked fields in a row and the third field is empty,
     # check the third one and block the opponent
-    if win_block(board, 0, 2) == "ok":
+    if win_block(fnboard, 0, 2) == "ok":
         return "ok"
     # 3. Do a fork - if you have two intersecting lines (horizontal, vertical, diagonal) with one your mark and with two
     # empty places AND if the intersection of the lines is empty, insert your mark in the intersection
     # (to create two possibilities to win in the next move)
-    for i in range(len(triples)):
-        xfld, ofld = count_xo(board, i)
+    for triple in range(len(triples)):
+        xfld, ofld = count_xo(fnboard, triple)
         if xfld == 1 and ofld == 0:  # first line with 1 computer mark and 0 user marks
-            for k in range(len(intersections[i])):  # checking all intersecting lines
-                l = intersections[i][k]  # number of intersecting line to check
-                xfldint, ofldint = count_xo(board, l)
+            for interfield in range(len(intersections[triple])):  # checking all intersecting lines
+                line_check = intersections[triple][interfield]  # number of intersecting line to check
+                xfldint, ofldint = count_xo(fnboard, line_check)
                 if xfldint == 1 and ofldint == 0:  # intersecting line with 1 computer mark and 0 user marks
-                    for n in range(3):
-                        for o in range(3):
-                            if triples[i][n] == triples[l][o]:
-                                x = triples[i][n][0]  # Coordinates of the checked field
-                                y = triples[i][n][1]
-                                if board[x][y] not in ("O", "X"):
-                                    board[x][y] = "X"
+                    for field in range(3):
+                        for lcheckfield in range(3):
+                            if triples[triple][field] == triples[line_check][lcheckfield]:
+                                x = triples[triple][field][0]  # Coordinates of the checked field
+                                y = triples[triple][field][1]
+                                if fnboard[x][y] not in ("O", "X"):
+                                    fnboard[x][y] = "X"
                                     return "ok"
     return
-    # 4. Do a blocking fork - if you have two intersecting lines (horizontal, vertical, diagonal) with one opponent mark
-    # and with two empty places AND if the intersection of the lines is empty, then: if there is empty place which would
-    # create two marks in a row for me (that the opponent has to block in his next move)
-    # - insert your mark in this place
+    # 4. Do a blocking fork if you have two intersecting lines (horizontal, vertical, diagonal) with one opponent mark
+    # and with two empty places AND if the intersection of the lines is empty, then:
+    # if there is empty place which would create two marks in a row for computer (that the opponent has to block
+    # in his next move) - insert your mark in this place
     # else: insert your mark in the intersection (to block two possibilities to win for the opponent in the next move)
 
-    for i in range(len(triples)):
-        xfld, ofld = count_xo(board, i)
-        if xfld == 1 and ofld == 0:  # first line with 1 computer mark and 0 user marks
-            for k in range(len(intersections[i])):  # checking all intersecting lines
-                l = intersections[i][k]  # number of intersecting line to check
-                xfldint, ofldint = count_xo(board, l)
+    for triple in range(len(triples)):
+        xfld, ofld = count_xo(fnboard, triple)
+        if xfld == 0 and ofld == 1:  # first line with 0 computer marks and 1 user mark
+            for interfield in range(len(intersections[triple])):  # checking all intersecting lines
+                line_check = intersections[triple][interfield]  # number of intersecting line to check
+                xfldint, ofldint = count_xo(fnboard, line_check)
                 if xfldint == 0 and ofldint == 1:  # intersecting line with 0 computer mark and 1 user marks
                     # To do: I think the algorithm has to be changed a little to avoid situation when after creating
                     # two own marks in a row, the opponent blocks it by inserting its mark in the intersection
@@ -159,16 +159,16 @@ def win_alg(board):
                     # So in such situation alg. have to check if there is a possibility to insert such mark
                     # in intersection, then if it can be placed out of intersection
                     # then marks the intersection without creating two in a row
-                    for p in range(len(triples)):  # To do: Here next - something wrong here
-                        xint, oint = count_xo(board, p)
+                    for p in range(len(triples)):  # To do: something wrong here
+                        xint, oint = count_xo(fnboard, p)
                         if xint == 1 and oint == 0:  # line to put 2nd mark
                             for n in range(3):  # Checking empty intersection there
                                 for o in range(3):
-                                    if triples[p][n] == triples[l][o]:
+                                    if triples[p][n] == triples[line_check][o]:
                                         x = triples[p][n][0]  # Coordinates of the checked field
                                         y = triples[p][n][1]
-                                        if board[x][y] not in ("O", "X"):
-                                            board[x][y] = "X"
+                                        if fnboard[x][y] not in ("O", "X"):
+                                            fnboard[x][y] = "X"
                                             return "ok"
 
     # 5. Use the center - if the center field is empty, insert your mark here
@@ -181,18 +181,17 @@ def win_alg(board):
     # 8. Play on the empty side - if there is an empty side field, insert your mark here
 
 
-
-def draw_move(board):  # Computer's move & board update
-    if win_alg(board) != "ok":
+def draw_move(fnboard):  # Computer's move & board update
+    if win_alg(fnboard) != "ok":
         print("random move")
         from random import randrange
-        free_fields = make_list_of_free_fields(board)
+        free_fields = make_list_of_free_fields(fnboard)
         if len(free_fields) != 0:
             comp_move = randrange(len(free_fields))
             field = free_fields[comp_move]  # tuple containing coordinates of computer move
             x = field[0]
             y = field[1]
-            board[x][y] = "X"
+            fnboard[x][y] = "X"
 
 
 # Initialization - empty board - list [row][column]
